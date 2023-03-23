c6f2504f66ed1d40001d895862fd6fa52f5df040
Jan Kaiser <honza.kaiser@gmail.com>
2023-03-23 13:00:40 +0100

    feat: add instructions with controls check

--- src/app/game/instructions/instructions.component.ts
+++ src/app/game/instructions/instructions.component.ts
@@ -1,17 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { CameraFeedDirective } from '../../controller/webcam/camera-feed.directive';
+import { Direction } from '../game.model';
+import { HandDetectorDirective } from '../../controller/webcam/hand-detector.directive';
 
 @Component({
   selector: 'snake-instructions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgClass, CameraFeedDirective, HandDetectorDirective],
   templateUrl: './instructions.component.html',
   styleUrls: ['./instructions.component.scss'],
 })
 export class InstructionsComponent {
   @Output() closeInstructions = new EventEmitter<void>();
 
+  @Input() public modal = false;
+
+  private controlsInitializedInternal = false;
+
+  public get controlsInitialized(): boolean {
+    return this.controlsInitializedInternal;
+  }
+
   public close(): void {
+    this.closeInstructions.emit();
+  }
+
+  public directionChange(direction: Direction): void {
+    if (direction === Direction.Right) {
+      this.controlsInitializedInternal = true;
+    }
+  }
+
+  public play(): void {
+    sessionStorage.setItem('controlsInitialized', 'true');
     this.closeInstructions.emit();
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebcamComponent } from '../controller/webcam/webcam.component';
 import { GameBoardComponent } from '../game/game-board/game-board.component';
 import { GameService } from '../game/game.service';
 import { ControlButtonsComponent } from '../controller/control-buttons/control-buttons.component';
 import { ControllerComponent } from '../controller/controller.component';
+import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Subject, takeUntil } from 'rxjs';
+import { InstructionsComponent } from '../game/instructions/instructions.component';
 
 @Component({
   selector: 'snake-home',
@@ -14,11 +18,14 @@ import { ControllerComponent } from '../controller/controller.component';
     ControllerComponent,
     GameBoardComponent,
     ControlButtonsComponent,
+    OverlayModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  private unsubscribe = new Subject<void>();
+
   @ViewChild(WebcamComponent)
   private webcamComponent?: WebcamComponent;
 
@@ -33,9 +40,42 @@ export class HomeComponent {
     return this.gameService.state$;
   }
 
-  public constructor(private gameService: GameService) {
+  public constructor(
+    private gameService: GameService,
+    private overlay: Overlay,
+  ) {
     this.gameService.setup({
       gridSize: { width: 22, height: 12 },
     });
+    if (sessionStorage.getItem('controlsInitialized') !== 'true') {
+      this.openInstructions();
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  private openInstructions(): void {
+    // open a modal with InstructionsComponent using Angular CDK overlay
+    const overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+      width: '90vw',
+      height: '96vh',
+    });
+    const componentRef = overlayRef.attach(
+      new ComponentPortal(InstructionsComponent),
+    );
+    componentRef.instance.modal = true;
+    componentRef.instance.closeInstructions
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => overlayRef.dispose());
   }
 }
